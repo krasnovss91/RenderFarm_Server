cee425d830ca70c8a349b860f56308cac69ae388
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-24 15:40:28 +0400

    RenderFarmServer

--- src/main/java/com/RenderFarmServerApp.java
+++ src/main/java/com/RenderFarmServerApp.java
@@ -1,4 +1,11 @@
 package com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RenderFarmServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RenderFarmServerApp.class, args);
+    }
 }
