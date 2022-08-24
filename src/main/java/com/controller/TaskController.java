facc636328373c03240cd3e95c40f078d1efc68f
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-24 17:25:32 +0400

    Controller

--- src/main/java/com/controller/TaskController.java
+++ src/main/java/com/controller/TaskController.java
@@ -1,4 +1,28 @@
 package com.controller;
 
+import com.model.Task;
+import com.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "renderFarmServer")
 public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks(){
+        return (List<Task>) taskRepository.findAll();
+    }
+
+    @GetMapping("saveTask")
+    public void saveTask(Task task) {
+        taskRepository.save(task);
+    }
 }
